c57e3c88f2a97f195da8ac52a6a023621ba8fd60
Matheus Castello <matheus@castello.eng.br>
2023-02-08 12:22:00 -0300

    scripts: generateBitBakeCtags: to work this need be a relative path
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- scripts/generateBitBakeCtags.sh
+++ scripts/generateBitBakeCtags.sh
@@ -2,6 +2,8 @@
 
 echo "BitBake Indexer 🏃 (Embedded Linux Dev)"
 
+cd $1
+
 # generate ctags for BitBake language
 ctags \
 	--langdef=BitBake \
@@ -12,18 +14,9 @@ ctags \
 	'--regex-BitBake=/^[ \t]*[a-zA-Z0-9_-]+[ \t]*\+?=[ \t]*\"[^\"]*/\0/v,variable/' \
 	--recurse=yes \
 	--languages=BitBake \
-	-f $1/.vscode-ctags \
-	$1 2> /dev/null
+	-f .vscode-ctags \
+	. 2> /dev/null
 
-# ctags \
-# 	--langdef=BitBake \
-# 	--langmap=BitBake:.bb.bbappend.inc.bbclass.conf \
-# 	'--regex-BitBake=/^[ \t]*(export|inherit)[ \t]+([a-zA-Z0-9_-]+)/\2/f,function/' \
-# 	'--regex-BitBake=/^[ \t]*([a-zA-Z0-9_-]+)[ \t]+?=[ \t]+/\1/v,variable/' \
-# 	'--regex-BitBake=/^[ \t]*[a-zA-Z0-9_-]+[ \t]*\+?=[ \t]*\"[^\"]*/\0/v,variable/' \
-# 	--recurse=yes \
-# 	--languages=BitBake \
-# 	-f $1/.vscode-ctags \
-# 	$1 2> /dev/null
+cd - > /dev/null
 
 echo "Done 😎 (Embedded Linux Dev)"
